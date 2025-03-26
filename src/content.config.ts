960d5f442e60a07a4eda9545cc7d21d7b95fd9cd
Aria Moradi <aria.moradi007@gmail.com>
2025-03-26 13:09:23 +0330

    get some basic features up and running

---
+++ src/content.config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from 'astro:content';
+import { glob, file } from 'astro/loaders';
+
+const index = defineCollection({ 
+    loader: glob({ pattern: "**/*.json", base: "./index" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        slug: z.string(),
+        date: z.string(),
+        tags: z.array(z.string()),
+        files: z
+            .object({
+                name: z.string(),
+                path: z.string(),
+                size: z.number(),
+                description: z.string().optional(),
+                telegram_file_id: z.string().optional(),
+            }).array(),
+    }).array(),
+});
+
+export const collections = { index };
